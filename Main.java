f8c2a42cf2850e5c368718ae672529168485fdd3
Igor <68941665+Rajskij@users.noreply.github.com>
2020-11-03 13:08:47 +0200

    release v0.1

--- Main.java
+++ Main.java
@@ -10,13 +10,30 @@ public class Main {
         Random random = new Random();
         char[] text = sc.nextLine().toCharArray();
         String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+        System.out.println(new String(text));
 
-        for (int i = 0; i < text.length; i += 3) {
-            int randIndex = i + random.nextInt(3);
-            if (randIndex < text.length) {
-                text[randIndex] = alphabet.charAt(random.nextInt(61));
+        char[] longText = new char[text.length * 3];
+        for (int i = 0; i < text.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                longText[i * 3 + j] = text[i];
             }
         }
-        System.out.println(new String(text));
+        System.out.println(new String(longText));
+
+        for (int i = 0; i < longText.length; i += 3) {
+            int randIndex = i + random.nextInt(3);
+            if (randIndex < longText.length) {
+                longText[randIndex] = alphabet.charAt(random.nextInt(61));
+            }
+        }
+        System.out.println(longText);
+
+        char[] txt = new char[longText.length / 3];
+        for (int i = 0; i < txt.length; i++) {
+            txt[i] = longText[i * 3] == longText[i * 3 + 1] ? longText[i * 3]
+                    : longText[i * 3 + 1] == longText[i * 3 + 2] ? longText[i * 3 + 1]
+                    : longText[i * 3 + 2];
+        }
+        System.out.println(new String(txt));
     }
 }
